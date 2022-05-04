247fce41e5869a1dc9ec95861ac0ea7f5bd2a204
Pier-Luc Richard <pier-lucrichard@Pier-Lucs-MacBook-Air.local>
2022-05-04 12:15:31 -0400

    still in codevault 2022-05-04

--- Unix_Threads_in_c_youtube/4-what_is_a_mutex.c/what_is_a_mutex.c
+++ Unix_Threads_in_c_youtube/4-what_is_a_mutex.c/what_is_a_mutex.c
@@ -5,42 +5,78 @@
 //in order you should compile with gcc -g -pthread shor_introduction_to_threads.c 
 #include <pthread.h>
 
-int mails = 0;
-
 /*
 	To solve the issue of the race condition
 	We can use a mutex
 
 	A mutex is a sort of a lock around a section of code
-	Below we set the variable lock at 0;
+	Here I initialize a mutez with the command 
+	pthread_mutex_t
+
+
+	Think as mutex as some bracket that
+	protects a part of a code that you want to protect
+	agaisnt other threads executing it at the same time
+	If at any point any thread is executing this line of code
+	Theres not gonna be any other thread aht is gonna execute this line of code
+	because there is a mutex around it (in our case mail++)
 */
-int lock = 0;
+int mails = 0;
+pthread_mutex_t	mutex;
+
 void	*routine()
 {
-	for (int i = 0;i < 100000; i++)
+	for (int i = 0;i < 1000000; i++)
 	{
+		// so checking if the lock have been taking by someone
+		// and wating for the lock to be unlocked
+		//and setting the lock to one once we are taking the play
+		//is completed by one function which is called 
+		// pthread_mutex_lock(&mutex)
+		pthread_mutex_lock(&mutex);
 		mails++;
+		//then instead of setting the lock to 0
+		// we use pthread_mutex_unlock passing the mutex
+		pthread_mutex_unlock(&mutex);
+
 	}
-	//step of incrementing
-	// read mails
-	// increment
-	// write mails
 	return (NULL);
 }
 
+
+/*
+	in order to use the mutex uptop and to lock the processes
+	we have to initialize it with pthread_mutex_init()
+	It takes two argument firt; the adress of the mutex
+	Second is some attribute we dont get to right now
+	NULL sets everything to default
+
+	NOTE THAT : at the end you have to use the function
+	pthread_mutex_destroy which takes the address to mutex
+	in order to destroy the created mutex 
+*/
 int main(int argc, char **argv)
 {
-	pthread_t t1, t2;
-
+	pthread_t t1, t2, t3 , t4;
+	pthread_mutex_init(&mutex, NULL);
 	if (pthread_create(&t1, NULL,&routine, NULL) != 0)
 		return (1);
 	if (pthread_create(&t2, NULL,&routine, NULL) != 0)
 		return (2);	
-	if (pthread_join(t1,NULL) != 0)
+	if (pthread_create(&t3, NULL,&routine, NULL) != 0)
 		return (3);
+	if (pthread_create(&t4, NULL,&routine, NULL) != 0)
+		return (4);	
+	if (pthread_join(t1,NULL) != 0)
+		return (5);
 	if (pthread_join(t2,NULL) != 0)
-		return (4);
+		return (6);
+	if (pthread_join(t3,NULL) != 0)
+		return (7);
+	if (pthread_join(t4,NULL) != 0)
+		return (8);
 	printf("Number of mails : %d\n", mails);
+	pthread_mutex_destroy(&mutex);
 	return (0);
 }
 
---
+++ Unix_Threads_in_c_youtube/5-create_threads_in_a_loop/how_to_create_threads_in_a_loop.c
@@ -0,0 +1,68 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <unistd.h>
+//To use Thread you need to inlcude pthread and also to link
+//in order you should compile with gcc -g -pthread shor_introduction_to_threads.c 
+#include <pthread.h>
+
+/*
+	To solve the issue of the race condition
+	We can use a mutex
+
+	A mutex is a sort of a lock around a section of code
+	Here I initialize a mutez with the command 
+	pthread_mutex_t
+
+
+	Think as mutex as some bracket that
+	protects a part of a code that you want to protect
+	agaisnt other threads executing it at the same time
+	If at any point any thread is executing this line of code
+	Theres not gonna be any other thread aht is gonna execute this line of code
+	because there is a mutex around it (in our case mail++)
+*/
+int mails = 0;
+pthread_mutex_t	mutex;
+
+void	*routine()
+{
+	for (int i = 0;i < 1000000; i++)
+	{
+		pthread_mutex_lock(&mutex);
+		mails++;
+		pthread_mutex_unlock(&mutex);
+
+	}
+	return (NULL);
+}
+
+
+/*
+	In the last videos we created thread one by one
+	repeating useless code multiple times
+
+	What we could do to eveitate that is
+	creating an array of threads
+*/
+int main(int argc, char **argv)
+{
+	pthread_t th[4];
+	pthread_mutex_init(&mutex, NULL);
+	for (int i = 0; i < 4; i++)
+	{
+		if (pthread_create(&th[i], NULL, &routine, NULL) != 0)
+		{
+			perror("Failed to create thread");
+			return (1);
+		}
+		printf("Thread %d has started\n", i);
+		if (pthread_join(th[i],NULL) != 0)
+			return (2);
+		printf("Thread %d has finished its execution\n", i);
+	}
+	printf("Number of mails : %d\n", mails);
+	pthread_mutex_destroy(&mutex);
+	return (0);
+}
+
+
