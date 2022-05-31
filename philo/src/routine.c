0587a043de4890efebc6819db929802ca196a83a
Pier-Luc Richard <pier-lucrichard@Pier-Lucs-MacBook-Air.local>
2022-05-31 15:25:21 -0400

    end of the day 31 may back to delay no more queue

--- philo/src/routine.c
+++ philo/src/routine.c
@@ -6,7 +6,7 @@
 /*   By: pirichar <pirichar@student.42quebec.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/05/19 10:44:32 by pirichar          #+#    #+#             */
-/*   Updated: 2022/05/31 12:11:04 by pirichar         ###   ########.fr       */
+/*   Updated: 2022/05/31 15:25:02 by pirichar         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -81,6 +81,7 @@ static void	sleep_routine(t_philo *p)
 // void	*routine(void *ptr)
 // {
 // 	t_philo	*p;
+	
 // 	p = ptr;
 // 	p->last_eaten = (get_time() - p->pgm->time.initial_time);
 // 	while (1)
@@ -91,8 +92,8 @@ static void	sleep_routine(t_philo *p)
 // 		{
 // 			if (check_full(p))
 // 				break;
-// 			if (p->pgm->game_over == false)
-// 				 take_forks_and_eat(p);
+// 			if (take_forks_and_eat(p) != 0)
+// 				break ;
 // 			if (check_full(p))
 // 				break;
 // 			sleep_routine(p);
@@ -107,7 +108,6 @@ static void	sleep_routine(t_philo *p)
 void	*routine(void *ptr)
 {
 	t_philo	*p;
-
 	p = ptr;
 	p->last_eaten = (get_time() - p->pgm->time.initial_time);
 	while (1)
@@ -115,7 +115,7 @@ void	*routine(void *ptr)
 		if (check_full(p))
 			break ;
 		if (p->id % 2 == 0)
-			usleep(p->pgm->time_to_die / 100);
+			usleep(p->pgm->time_to_die / 10);
 		if (take_forks_and_eat(p) != 0)
 			break ;
 		if (check_full(p))
